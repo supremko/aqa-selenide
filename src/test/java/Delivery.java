54af92da5d515957136a1a0d45b9f8d5adbc3cab
Kirill Lepilov <supremko.fun@gmail.com>
2019-12-17 21:30:33 +0300

    update test

--- src/test/java/Delivery.java
+++ src/test/java/Delivery.java
@@ -1,35 +1,44 @@
 import com.codeborne.selenide.*;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.Keys;
 
-//import java.text.DateFormat;
-//import java.text.SimpleDateFormat;
-//import java.time.LocalDate;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class Delivery {
-//    DateFormat dtf = SimpleDateFormat.getDateInstance();
-//    LocalDate localDate = LocalDate.now();
+    LocalDate localDate = LocalDate.now();
 
     @Test
     void reserveDelivery() {
         open("http://localhost:9999");
         SelenideElement form = $("form");
         form.$("[placeholder='Город']").setValue("Санкт-Петербург");
-//        Тест падает при попытке перевода в дату, не получилось =(
-//        int deliveryDay = Integer.parseInt(dtf.format(form.$("[placeholder='Дата встречи']").getValue()));
-//        int localDay = Integer.parseInt(dtf.format(localDate.plusDays(3)));
-//        if (deliveryDay < localDay){
-//            form.$("[placeholder='Дата встречи']").setValue(localDate.plusDays(3).toString());
-//        }
+        form.$("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        form.$("[placeholder='Дата встречи']").setValue(localDate.plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.YYYY")));
         form.$("[name='name']").setValue("Джон Коннор");
         form.$("[name='phone']").setValue("+79990000000");
         form.$("[data-test-id=agreement]").click();
         form.$("[class='button__content']").click();
         $(withText("Встреча успешно забронирована на")).waitUntil(visible, 15000);
+    }
 
+    @Test
+    void reserveDelivery2() {
+//        localDate = LocalDate.now();
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[placeholder='Город']").setValue("Санкт-Петербург");
+        form.$("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        form.$("[placeholder='Дата встречи']").setValue(localDate.format(DateTimeFormatter.ofPattern("dd.MM.YYYY")));
+        form.$("[name='name']").setValue("Джон Коннор");
+        form.$("[name='phone']").setValue("+79990000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[class='button__content']").click();
+        form.$(withText("Заказ на выбранную дату невозможен")).shouldBe(visible);
     }
 
 }
